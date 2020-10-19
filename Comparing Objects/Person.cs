854a29576213e7f47d906db04415ac9e9a7543a8
TrifShterev <tshterev@yahoo.com>
2020-10-19 20:45:12 +0300

    ComparingObjects

---
+++ Comparing Objects/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Comparing_Objects
+{
+  public  class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+        public Person(string name, int age , string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            int result = 1;
+            if (other!=null)
+            {
+                result = other.name.CompareTo(this.name);
+                if (result==0)
+                {
+                    result = other.age.CompareTo(this.age);
+                    if (result==0)
+                    {
+                        result = other.town.CompareTo(this.town);
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Comparing Objects/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Comparing_Objects
+{
+  public  class StartUp
+    {
+      public  static void Main(string[] args)
+        {
+           
+            const string END_COMMAND = "END";
+
+            List<Person> people = new List<Person>();
+            string command;
+            while ((command=Console.ReadLine())!=END_COMMAND)
+            {
+                string[] cmdArgs = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                string name = cmdArgs[0];
+                int age = int.Parse(cmdArgs[1]);
+                string town = cmdArgs[2];
+
+                Person p = new Person(name, age, town);
+                people.Add(p);
+            }
+            int n = int.Parse(Console.ReadLine());
+
+            Person comparedPerson = people[n - 1];
+            int samePersonCount = 0;
+            foreach (var person in people)
+            {
+                if (person.CompareTo(comparedPerson)==0)
+                {
+                    samePersonCount++;
+                }
+            }
+            if (samePersonCount==1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                int notSamePersonCount = people.Count - samePersonCount;
+                Console.WriteLine($"{samePersonCount} " +
+                    $"{notSamePersonCount} {people.Count}");
+            }
+
+        }
+    }
+}
