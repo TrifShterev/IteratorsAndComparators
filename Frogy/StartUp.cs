d98a7bd6f3ffb63d982f9db29d4642592b73fdad
TrifShterev <tshterev@yahoo.com>
2020-10-19 19:40:22 +0300

    Add project files.

---
+++ Frogy/StartUp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Frogy
+{
+  public  class StartUp
+    {
+     public   static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()?
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            Lake lake = new Lake(stones);
+            List<int> path = new List<int>();
+
+            foreach (var stone in lake)
+            {
+                path.Add(stone);
+            }
+            Console.WriteLine( string.Join(", ", path));
+        }
+    }
+}
